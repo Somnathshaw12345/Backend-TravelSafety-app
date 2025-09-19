3904177d2d8339a004aa1de3c7933f8ac6d3df7e
somnath shaw <somnathshaw067@gmail.com>
2025-09-19 17:19:57 +0530

    fourth commit

--- travelSafety/safezone/urls.py
+++ travelSafety/safezone/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("signup/", views.signup_view, name="signup"),
-    path("", views.login_view, name="login"),   #login page
+    path("", views.signup_view, name="signup"),
+    path("login/", views.login_view, name="login"),   #login page
     path("user/dashboard/", views.user_dashboard, name="user_dashboard"),
     path("admin/dashboard/", views.admin_dashboard, name="admin_dashboard"),
     path("logout/", views.logout_view, name="logout"),
